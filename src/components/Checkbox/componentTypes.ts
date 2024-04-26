272ad52e64bd6d743140b14b6c4287f13e7579c7
WW <weswithley@gmail.com>
2024-04-26 20:14:49 +0800

    Add component Checkbox

---
+++ src/components/Checkbox/componentTypes.ts
@@ -0,0 +1,14 @@
+export type ClassNamesType = Array<string | null | undefined>;
+export type ClassNameType = string;
+export type StylesType = React.CSSProperties;
+export type CheckedType = boolean;
+export type DisabledType = boolean;
+export type OnChangeType = () => void;
+
+export interface ComponentInterface {
+  className?: ClassNameType;
+  styles?: StylesType;
+  checked: CheckedType;
+  disabled: DisabledType;
+  onClick: OnChangeType;
+}
