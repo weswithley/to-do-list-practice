458824f1fdfbcde319503e0d3c7bc8b64517e75e
WW <weswithley@gmail.com>
2024-04-26 10:54:00 +0800

    ENV done

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './assets/scss/global.scss';
+
+const App = () => (
+  <div className="to_to_list_container">
+    This is app.
+  </div>
+)
+
+export default App;
