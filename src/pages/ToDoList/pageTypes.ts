c41cbaf917c5fa9466f03b288ab9571cfe28164f
WW <weswithley@gmail.com>
2024-05-01 17:35:25 +0800

    Enhancement > Page ToDoList : 1. Layout style and RWD layout style. 2. Modify the namspace of controller.ts to hook.ts and change the usage of customize function to hook function. 3. Combine all the customize functions into one hook function. 4. Modify the const and add types into it. 5. Use Font awesome icon instead of button with label string. 6. Add more types into pageTypes.

--- src/pages/ToDoList/pageTypes.ts
+++ src/pages/ToDoList/pageTypes.ts
@@ -1,11 +1,19 @@
+// Components types
+import {
+  FilterSearchInterface
+} from '../../components/FilterSearchDropdown/componentTypes';
+
 // To do List
 export type IdType = number;
 export type ValuesType = string | number;
 export type CheckedType = boolean;
 export type IsSearchMatchType = boolean;
 export type IsFilterMatchType = boolean;
-export type CreateTimeType = string;
+export type CreateTimeType = Date | null;
+export type createTimeLocaleStringType = string;
 export type CreateTimeStampType = number;
+export type UpdateKeyType = string;
+export type UpdateValueType = string | number | boolean | null | undefined;
 
 export interface ToDoInterface {
   id: IdType;
@@ -14,24 +22,30 @@ export interface ToDoInterface {
   isSearchMatch: IsSearchMatchType;
   isFilterMatch: IsFilterMatchType;
   createTime: CreateTimeType;
+  createTimeLocaleString: createTimeLocaleStringType,
   createTimeStamp: CreateTimeStampType;
 }
+
+export interface PageInterface {}
 
 // Search
 export type KeywordType = string;
 export type FilterType = string;
+export type SearchKeyType = string;
+export type SearchValueType = string;
 export interface SearchInterface {
   keyword: KeywordType;
   filter: FilterType;
 }
 
-// Sort
-export type IsSortType = boolean;
-
-// Page
-export interface PageInterface {}
+// Filter search
 export interface FilterConditionsInterface {
   All: (target: ToDoInterface) => boolean;
   Completed: (target: ToDoInterface) => boolean;
   UnCompleted: (target: ToDoInterface) => boolean;
 }
+
+export interface FilterConditionsInterfaceEnum extends FilterSearchInterface {}
+
+// Sort
+export type IsSortType = boolean | null;
